3048134db62a4ec2e3b0e9b8e51610a69c90b0c8
7singhakshat <7singhakshat@gmail.com>
2025-01-01 22:27:10 +0530

    News App

--- app/src/main/java/com/example/newsapp/presentation/screens/HomeScreenUI.kt
+++ app/src/main/java/com/example/newsapp/presentation/screens/HomeScreenUI.kt
@@ -1,5 +1,6 @@
 package com.example.newsapp.presentation.screens
 
+import android.view.RoundedCorner
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
@@ -25,6 +27,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,50 +57,62 @@ fun HomeScreenUI(
     } else if (state.value.error.isNullOrBlank().not()) {
         Text(text = state.value.error.toString())
     } else {
-        Column (modifier=Modifier.fillMaxSize()){ Row {
-            OutlinedTextField(value = searchTerm.value, onValueChange = {
-                searchTerm.value = it
-            }, placeholder = {Text(text = "Search")})
-            IconButton(onClick = {
-                viewModel.getEverything(q = searchTerm.value)
-            }, enabled = searchTerm.value.isNullOrBlank().not()) {
-                Icon(imageVector = Icons.Default.Search, contentDescription = null)
-            }
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row {
+                OutlinedTextField(modifier= modifier.fillMaxWidth().padding(20.dp,0.dp,20.dp,5.dp),
+                    shape = RoundedCornerShape(20.dp),value = searchTerm.value, onValueChange = {
+                    searchTerm.value = it
+                }, placeholder = { Text(text = "Search") },
+                    label = { Text(text = "Search") },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search, contentDescription = null,
+                            modifier = modifier.clickable(
+                                enabled = searchTerm.value.isNullOrBlank().not(),
+                                onClick = { viewModel.getEverything(q = searchTerm.value) }
 
-        }
+                            ))
+                    })
+
+            }
             val data = state.value.data
             if (data?.articles!!.isEmpty()) {
                 Text(text = "No Data Found")
             } else {
 
-                val articles = data.articles.filter { article->
-                    article.title?.contains("Removed")==false
+                val articles = data.articles.filter { article ->
+                    article.title?.contains("Removed") == false
                 }
                 LazyColumn(modifier = Modifier.fillMaxSize()) {
                     items(articles) { article ->
                         Card(modifier = Modifier
                             .fillMaxWidth()
                             .wrapContentHeight()
-                            .padding(8.dp).clickable {
-                                navController.navigate(CategoryScreen(
-                                    author = article.author,
-                                    content = article.content,
-                                    description = article.description,
-                                    publishedAt = article.publishedAt,
-                                    id= article.source!!.id,
-                                    name = article.source.name,
-                                    title = article.title,
-                                    url = article.url,
-                                    urlToImage = article.urlToImage
+                            .padding(8.dp)
+                            .clickable {
+                                navController.navigate(
+                                    CategoryScreen(
+                                        author = article.author,
+                                        content = article.content,
+                                        description = article.description,
+                                        publishedAt = article.publishedAt,
+                                        id = article.source!!.id,
+                                        name = article.source.name,
+                                        title = article.title,
+                                        url = article.url,
+                                        urlToImage = article.urlToImage
 
-                                ))
+                                    )
+                                )
                             }) {
                             Column {
                                 if (article.urlToImage.isNullOrBlank()) {
-                                Image(painter = painterResource(R.drawable.ic_launcher_foreground), contentDescription = null)
+                                    Image(
+                                        painter = painterResource(R.drawable.ic_launcher_foreground),
+                                        contentDescription = null
+                                    )
 
-                                }
-                                else{
+                                } else {
                                     AsyncImage(
                                         model = article.urlToImage,
                                         contentDescription = null,
